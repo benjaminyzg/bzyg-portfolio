8ff35f4d33999810fa0a44fc513f1899ea81047d
benjaminyzg <benjamin.yong.sg@gmail.com>
2026-06-11 07:42:46 +0800

    moving project to separate repository

--- benjaminyzg.github.io/vite.config.js
+++ benjaminyzg.github.io/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Use '/' for username.github.io, or '/repo-name/' for a sub-project
 })
