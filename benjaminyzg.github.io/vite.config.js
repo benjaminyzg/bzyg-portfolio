7252072d663ffa50a1373c07489d8578f0d63de2
benjaminy_zg <19578848+benjaminyzg@users.noreply.github.com>
2026-07-02 13:31:59 +0800

    Update vite.config.js

--- benjaminyzg.github.io/vite.config.js
+++ benjaminyzg.github.io/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin'
+import react from '@vitejs/react-refresh' // or @vitejs/plugin-react depending on your exact package
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/react-portfolio/', // 👈 ADD THIS EXACT LINE
+  base: '/react-portfolio/',
+  plugins: [react()]
 })
